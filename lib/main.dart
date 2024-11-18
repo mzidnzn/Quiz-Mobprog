9de4072036dd91370b76f4f095bc7b765663cf89
ZiddanZid <161700854+mzidnzn@users.noreply.github.com>
2024-11-18 16:47:07 +0700

    ganti main screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,281 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      theme: themeProvider.selectedTheme,
+      home: const MainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+// Main Screen
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade300, Colors.purple.shade400],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome to Flutter!',
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: themeProvider.selectedFont,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 10.0,
+                      color: Colors.black26,
+                      offset: Offset(3, 3),
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  backgroundColor: themeProvider.selectedTheme.colorScheme.primary,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  elevation: 10,
+                  shadowColor: Colors.black45,
+                ),
+                icon: Icon(Icons.settings, color: Colors.white),
+                label: const Text(
+                  'Go to Settings',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    letterSpacing: 1.2,
+                    fontFamily: 'Roboto',
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Settings Screen
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(
+          'Settings',
+          style: TextStyle(fontFamily: themeProvider.selectedFont),
+        ),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
             const Text(
-              'You have pushed the button this many times:',
+              'Choose a Theme:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ...ThemeType.values.map(
+              (theme) => RadioListTile(
+                title: Text(
+                  theme.name,
+                  style: const TextStyle(fontSize: 16),
+                ),
+                value: theme,
+                groupValue: themeProvider.themeType,
+                onChanged: themeProvider.updateTheme,
+              ),
+            ),
+            const Divider(height: 30),
+            const Text(
+              'Choose a Font:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            DropdownButton<String>(
+              value: themeProvider.selectedFont,
+              isExpanded: true,
+              items: themeProvider.fonts
+                  .map(
+                    (font) => DropdownMenuItem(
+                      value: font,
+                      child: Text(font, style: TextStyle(fontFamily: font)),
+                    ),
+                  )
+                  .toList(),
+              onChanged: themeProvider.updateFont,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+// Theme Provider
+class ThemeProvider extends ChangeNotifier {
+  ThemeType _themeType = ThemeType.light;
+  String _selectedFont = 'Roboto';
+
+  final List<String> fonts = [
+    'Roboto',
+    'Lobster',
+    'Courier',
+    'OpenSans',
+    'DancingScript',
+    'Poppins',
+    'Montserrat',
+    'Pacifico',
+    'Raleway',
+    'Merriweather',
+    'Nunito',
+    'Oswald',
+    'RobotoSlab',
+    'Cinzel',
+    'AbrilFatface',
+    'BebasNeue',
+    'PlayfairDisplay',
+    'ShadowsIntoLight',
+    'SourceSansPro',
+  ];
+
+  ThemeType get themeType => _themeType;
+  String get selectedFont => _selectedFont;
+
+  ThemeData get selectedTheme {
+    switch (_themeType) {
+      case ThemeType.dark:
+        return ThemeData.dark().copyWith(
+          colorScheme: ColorScheme.dark(primary: Colors.grey.shade800),
+          textTheme: _getTextTheme(Colors.white),
+        );
+      case ThemeType.blue:
+        return ThemeData(
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.blue.shade50,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.blue),
+          textTheme: _getTextTheme(Colors.blue.shade900),
+        );
+      case ThemeType.green:
+        return ThemeData(
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: Colors.green.shade50,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.green),
+          textTheme: _getTextTheme(Colors.green.shade900),
+        );
+      case ThemeType.purple:
+        return ThemeData(
+          primarySwatch: Colors.purple,
+          scaffoldBackgroundColor: Colors.purple.shade50,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.purple),
+          textTheme: _getTextTheme(Colors.purple.shade900),
+        );
+      case ThemeType.orange:
+        return ThemeData(
+          primarySwatch: Colors.orange,
+          scaffoldBackgroundColor: Colors.orange.shade50,
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.orange),
+          textTheme: _getTextTheme(Colors.orange.shade900),
+        );
+      default:
+        return ThemeData.light().copyWith(
+          textTheme: _getTextTheme(Colors.black),
+        );
+    }
+  }
+
+  void updateTheme(ThemeType? theme) {
+    if (theme != null) {
+      _themeType = theme;
+      notifyListeners();
+    }
+  }
+
+  void updateFont(String? font) {
+    if (font != null) {
+      _selectedFont = font;
+      notifyListeners();
+    }
+  }
+
+  TextTheme _getTextTheme(Color color) {
+    return TextTheme(
+      bodyLarge: TextStyle(color: color, fontFamily: _selectedFont),
+      bodyMedium: TextStyle(color: color, fontFamily: _selectedFont),
+      titleLarge: TextStyle(color: color, fontFamily: _selectedFont),
+    );
+  }
+}
+
+enum ThemeType { light, dark, blue, green, purple, orange }
+
+// Extension to format theme names
+extension ThemeTypeExtension on ThemeType {
+  String get name {
+    switch (this) {
+      case ThemeType.light:
+        return 'Light Theme';
+      case ThemeType.dark:
+        return 'Dark Theme';
+      case ThemeType.blue:
+        return 'Blue Theme';
+      case ThemeType.green:
+        return 'Green Theme';
+      case ThemeType.purple:
+        return 'Purple Theme';
+      case ThemeType.orange:
+        return 'Orange Theme';
+    }
   }
 }
